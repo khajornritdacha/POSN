de63125872ea177f66e8880ce263068f5b4be2a5
Vijak Khajornritdacha <vijak13019@hotmail.com>
2025-03-11 11:25:08 +0700

    add min heap

--- DataStructure/stl/priority_queue/priority_queue.cpp
+++ DataStructure/stl/priority_queue/priority_queue.cpp
@@ -2,14 +2,20 @@
 using namespace std;
 
 int main() {
+    //push, top, pop, size, empty
+    //Priority_queue is a heap
+    //By default it is a max heap
+    
     priority_queue<int> pq;
 
     for (int i = 1; i <= 5; i++) {
         pq.push(i); // Add elements to PQ
     }
-
+    
     cout << "Size: " << pq.size() << "\n";
-
+    cout << "Maximum element : " << pq.top() << "\n";
+    
+    cout << "Iterate through pq (Get element in decreasing order)\n";
     while (!pq.empty()) { // Check if pq is empty
         cout << pq.top() << "\n"; // Print the maximum value
         pq.pop(); // Remove the maximum value
